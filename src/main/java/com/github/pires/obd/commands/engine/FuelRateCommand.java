6ce9606b6a9fb87ed34a327883dd032d01e20c6e
John of Saints <johnofsaints@gmail.com>
2015-09-19 12:27:24 +0400

    Update FuelRateCommand.java
    Correct method name from "getVoltage" to "getFuelRate"

--- src/main/java/com/github/pires/obd/commands/engine/FuelRateCommand.java
+++ src/main/java/com/github/pires/obd/commands/engine/FuelRateCommand.java
@@ -64,7 +64,7 @@ public class FuelRateCommand extends ObdCommand {
     /**
      * @return a double.
      */
-    public double getVoltage() {
+    public double getFuelRate() {
         return fuelrate;
     }
 
